dffd8b717ad9d4567a177e392fda88c9b846770a
natechols <nechols@pacificbiosciences.com>
2016-02-03 12:10:45 -0800

    migrating conversion tool tasks from pbsmrtpipe

--- regenerate-tool-contracts.py
+++ regenerate-tool-contracts.py
@@ -15,12 +15,16 @@ def run(args):
     module_dir = os.path.join(os.path.dirname(__file__), "pbcoretools", "tasks")
     for file_name in os.listdir(module_dir):
         if file_name.endswith(".py") and not file_name.startswith("_"):
+            if file_name in ["converters.py"]:
+                continue
             module_name = "pbcoretools.tasks.{m}".format(m=file_name[:-3])
             json_file = os.path.join(output_dir,
                 "{m}_tool_contract.json".format(m=module_name))
             cmd = "python -m {m} --emit-tool-contract > {j}".format(
                 m=module_name, j=json_file)
             run_cmd(cmd, sys.stdout, sys.stderr)
+    cmd = "python -m pbcoretools.tasks.converters emit-tool-contracts -o {d}".format(d=output_dir)
+    run_cmd(cmd, sys.stdout, sys.stderr)
 
 if __name__ == "__main__":
     run(sys.argv[1:])
