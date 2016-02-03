f768f81a6959dc8a4b7f1aee51e7349693c3293f
natechols <nechols@pacificbiosciences.com>
2016-02-03 10:15:51 -0800

    moved scatter/gather tools from pbsmrtpipe

---
+++ regenerate-tool-contracts.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import subprocess
+import sys
+import os
+
+from pbcommand.engine import run_cmd
+
+
+def run(args):
+    output_dir = os.getcwd()
+    if len(args) == 1:
+        output_dir = args[0]
+        assert os.path.isdir(output_dir), "Not a directory: %s"%output_dir
+    module_dir = os.path.join(os.path.dirname(__file__), "pbcoretools", "tasks")
+    for file_name in os.listdir(module_dir):
+        if file_name.endswith(".py") and not file_name.startswith("_"):
+            module_name = "pbcoretools.tasks.{m}".format(m=file_name[:-3])
+            json_file = os.path.join(output_dir,
+                "{m}_tool_contract.json".format(m=module_name))
+            cmd = "python -m {m} --emit-tool-contract > {j}".format(
+                m=module_name, j=json_file)
+            run_cmd(cmd, sys.stdout, sys.stderr)
+
+if __name__ == "__main__":
+    run(sys.argv[1:])
---
+++ tests/unit/base.py
@@ -0,0 +1,15 @@
+
+import tempfile
+
+def _get_temp_file(suffix, dir_):
+    t = tempfile.NamedTemporaryFile(suffix=suffix, delete=False, dir=dir_)
+    t.close()
+    return t.name
+
+
+def get_temp_file(suffix="", dir_=None):
+    return _get_temp_file(suffix, dir_=dir_)
+
+def get_temp_dir(suffix=""):
+    """This will make subdir in the root tmp dir"""
+    return tempfile.mkdtemp(dir=None, suffix=suffix)
