f0ecfb3adb9cfc4b2aed918e4267246b3e9abb5e
natechols <nathaniel.echols@gmail.com>
2017-03-23 08:55:25 -0700

    make h5py optional

--- bamboo_build.sh
+++ bamboo_build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -ex
 
 source /mnt/software/Modules/current/init/bash
-module load hdf5-tools/1.8.16
 module load python/2.7.9
 
 mkdir -p tmp/bin
@@ -22,7 +21,6 @@ fi
 
 (cd repos/PacBioTestData && make python)
 (cd repos/pbcommand && make install)
-pip install --no-cache-dir h5py
 pip install -r requirements-ci.txt
 (cd repos/pbcore && make install)
 pip install -r requirements-dev.txt
