3b641a36518d741e063bddb055addf5bae0c11a4
Mengjuei Hsieh <mhsieh@pacificbiosciences.com>
2017-04-03 14:40:22 -0700

    using the artifacts

--- bamboo_build.sh
+++ bamboo_build.sh
@@ -1,29 +1,43 @@
 #!/bin/bash -ex
 
-source /mnt/software/Modules/current/init/bash
-module load python/2.7.9
+NX3PBASEURL=http://nexus.pacificbiosciences.com/repository/maven-thirdparty/gcc-4.9.2
+export PATH=$PWD/bin:/mnt/software/a/anaconda2/4.2.0/bin:$PATH
+export PYTHONUSERBASE=$PWD
+PIP="pip --cache-dir=$bamboo_build_working_directory/.pip"
+type module >& /dev/null || . /mnt/software/Modules/current/init/bash
+module load gcc/4.9.2
 
-mkdir -p tmp/bin
-python /mnt/software/v/virtualenv/13.0.1/virtualenv.py tmp/venv
-source tmp/venv/bin/activate
+rm -rf bin lib include share
+mkdir  bin lib include share
+
 # HACK to put binaries on path
 if [ ! -z "$PB_TOOLS_BIN" ]; then
   echo "Symlinking to executables in smrttools installation..."
-  ln -s $PB_TOOLS_BIN/pbindex tmp/bin
-  ln -s $PB_TOOLS_BIN/pbmerge tmp/bin/
-  ln -s $PB_TOOLS_BIN/bax2bam tmp/bin
-  ln -s $PB_TOOLS_BIN/bam2bam tmp/bin
-  ln -s $PB_TOOLS_BIN/samtools tmp/bin
-  export PATH=$PATH:$PWD/tmp/bin
+  ln -sfn $PB_TOOLS_BIN/pbindex  bin/
+  ln -sfn $PB_TOOLS_BIN/pbmerge  bin/
+  ln -sfn $PB_TOOLS_BIN/bax2bam  bin/
+  ln -sfn $PB_TOOLS_BIN/bam2bam  bin/
+  ln -sfn $PB_TOOLS_BIN/samtools bin/
 else
   echo "WARNING: smrttools not available, some tests will be skipped"
 fi
 
-(cd repos/PacBioTestData && make python)
-(cd repos/pbcommand && make install)
-pip install -r requirements-ci.txt
-(cd repos/pbcore && make install)
-pip install -r requirements-dev.txt
+$PIP install --user \
+  iso8601
+$PIP install --user \
+  $NX3PBASEURL/pythonpkgs/xmlbuilder-1.0-cp27-none-any.whl \
+  $NX3PBASEURL/pythonpkgs/tabulate-0.7.5-cp27-none-any.whl \
+  $NX3PBASEURL/pythonpkgs/pysam-0.9.1.4-cp27-cp27mu-linux_x86_64.whl \
+  $NX3PBASEURL/pythonpkgs/avro-1.7.7-cp27-none-any.whl
 
-python setup.py install
+(cd repos/PacBioTestData && make python)
+
+(cd repos/pbcommand && make install)
+(cd repos/pbcore && make install)
+
+$PIP install --user -r requirements-ci.txt
+$PIP install --user -r requirements-dev.txt
+#python setup.py install
+$PIP install --user -e ./
+
 make test
