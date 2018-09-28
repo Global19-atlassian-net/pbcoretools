935bc2d098b6461aebd456570523e925b6c69b42
natechols <nathaniel.echols@gmail.com>
2018-09-27 21:59:52 -0700

    add unit test

--- tests/unit/test_tasks_auto_ccs_outputs.py
+++ tests/unit/test_tasks_auto_ccs_outputs.py
@@ -3,6 +3,7 @@ import unittest
 import tempfile
 import logging
 import os.path as op
+import sys
 
 from pbcommand.testkit import PbTestApp
 from pbcommand.models import DataStore
@@ -20,13 +21,26 @@ class TestAutoCCSOutputs(PbTestApp):
         op.join(TESTDATA, "auto_ccs_outputs/m54006_180707_211919.consensusreadset.xml")
     ]
 
-    def run_after(self, rtc, output_dir):
-        ds = DataStore.load_from_json(rtc.task.output_files[0])
-        file_names = sorted([op.basename(f.path) for f in ds.files.values()])
+    def setUp(self):
+        # FIXME workaround for 'nose' stupidity
+        sys.stdout = sys.__stdout__
+        sys.stderr = sys.__stderr__
+
+    def _check_datastore_files(self, files):
+        file_names = sorted([op.basename(f.path) for f in files])
         self.assertEqual(file_names, [
             "m54006_180707_211919.Q20.fastq",
             "m54006_180707_211919.ccs.bam"
         ])
+
+    def run_after(self, rtc, output_dir):
+        ds = DataStore.load_from_json(rtc.task.output_files[0])
+        self._check_datastore_files(ds.files.values())
+
+    def test_run_ccs_bam_fastq_exports(self):
+        tmp_dir = tempfile.mkdtemp()
+        files = run_ccs_bam_fastq_exports(self.INPUT_FILES[0], tmp_dir)
+        self._check_datastore_files(files)
 
 
 @skip_if_no_testdata
