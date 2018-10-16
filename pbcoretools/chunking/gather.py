9a1514bffc57e7f17a0edf364142b9c856d47ed6
natechols <nathaniel.echols@gmail.com>
2018-10-16 14:15:48 -0700

    bug fix

--- pbcoretools/chunking/gather.py
+++ pbcoretools/chunking/gather.py
@@ -196,9 +196,9 @@ def gather_fofn(input_files, output_file, skip_empty=True):
 def _sanitize_dataset_tags(dset):
     tags = set(dset.tags.split(","))
     if "chunked" in tags or "filtered" in tags:
-        x.discard("chunked")
-        x.discard("filtered")
-        dset.tags = ",".join(sorted(list(dset.tags)))
+        tags.discard("chunked")
+        tags.discard("filtered")
+        dset.tags = ",".join(sorted(list(tags)))
 
 
 def __gather_contigset(resource_file_extension, input_files, output_file,
@@ -371,3 +371,156 @@ def gather_zip(input_files, output_file):
                     f = zip_in.open(member)
                     zip_out.writestr(member, f.read())
     return output_file
+
+
+def __add_chunk_key_option(default_chunk_key):
+    def _add_chunk_key_option(p):
+        p.add_argument('--chunk-key', type=str, default=default_chunk_key,
+                       help="Chunk key (e.g, $chunk.my_chunk_key_id) to gather over")
+        return p
+    return _add_chunk_key_option
+
+
+add_chunk_key_csv = __add_chunk_key_option('$chunk.csv_id')
+add_chunk_key_txt = __add_chunk_key_option('$chunk.txt_id')
+add_chunk_key_fasta = __add_chunk_key_option('$chunk.fasta_id')
+add_chunk_key_fastq = __add_chunk_key_option('$chunk.fastq_id')
+add_chunk_key_gff = __add_chunk_key_option('$chunk.gff_id')
+add_chunk_key_vcf = __add_chunk_key_option('$chunk.vcf_id')
+add_chunk_key_fofn = __add_chunk_key_option('$chunk.fofn_id')
+add_chunk_key_subreadset = __add_chunk_key_option('$chunk.subreadset_id')
+add_chunk_key_alignmentset = __add_chunk_key_option('$chunk.alignmentset_id')
+add_chunk_key_ccsset = __add_chunk_key_option('$chunk.ccsset_id')
+add_chunk_key_ccs_alignmentset = __add_chunk_key_option(
+    '$chunk.ccs_alignmentset_id')
+add_chunk_key_transcripts = __add_chunk_key_option('$chunk.transcriptset_id')
+# TODO: change this to contigset_id once quiver emits contigsets
+add_chunk_key_contigset = __add_chunk_key_option('$chunk.fasta_id')
+add_chunk_key_report = __add_chunk_key_option('$chunk.json_id')
+add_chunk_key_bigwig = __add_chunk_key_option('$chunk.bw_id')
+add_chunk_key_tgz = __add_chunk_key_option('$chunk.tgz_id')
+
+
+def __gather_options(output_file_message, input_files_message, input_validate_func, add_chunk_key_func_):
+    def _f(p):
+        p.add_argument('chunk_json',
+                       type=input_validate_func,
+                       help=input_files_message)
+        p.add_argument('-o', '--output', type=str, help=output_file_message)
+        return add_chunk_key_func_(p)
+    return _f
+
+
+def __add_gather_options(output_file_msg, input_file_msg, chunk_key_func):
+    def _f(p):
+        add_debug_option(p)
+        f = __gather_options(output_file_msg, input_file_msg,
+                             validate_file, chunk_key_func)
+        return f(p)
+    return _f
+
+
+_gather_csv_options = __add_gather_options(
+    "Output CSV file", "input CSV file", add_chunk_key_csv)
+_gather_txt_options = __add_gather_options(
+    "Output text file", "input text file", add_chunk_key_txt)
+_gather_report_options = __add_gather_options(
+    "Output JSON file", "input JSON file", add_chunk_key_report)
+_gather_fastq_options = __add_gather_options(
+    "Output Fastq file", "Chunk input JSON file", add_chunk_key_fastq)
+_gather_fasta_options = __add_gather_options(
+    "Output Fasta file", "Chunk input JSON file", add_chunk_key_fasta)
+_gather_gff_options = __add_gather_options(
+    "Output GFF file", "Chunk input JSON file", add_chunk_key_gff)
+_gather_vcf_options = __add_gather_options(
+    "Output VCF file", "Chunk input JSON file", add_chunk_key_vcf)
+_gather_fofn_options = __add_gather_options(
+    "Output Fofn file", "Chunk input JSON file", add_chunk_key_fofn)
+_gather_subreadset_options = __add_gather_options("Output SubreadSet XML file",
+                                                  "Chunk input JSON file",
+                                                  add_chunk_key_subreadset)
+_gather_ccsset_options = __add_gather_options("Output ConsensusReadSet XML file",
+                                              "Chunk input JSON file",
+                                              add_chunk_key_ccsset)
+_gather_alignmentset_options = __add_gather_options("Output AlignmentSet XML file",
+                                                    "Chunk input JSON file",
+                                                    add_chunk_key_alignmentset)
+_gather_ccs_alignmentset_options = __add_gather_options("Output ConsensusAlignmentSet XML file",
+                                                        "Chunk input JSON file",
+                                                        add_chunk_key_ccs_alignmentset)
+_gather_transcripts_options = __add_gather_options("Output TranscriptSet XML file",
+                                                   "Chunk input JSON file",
+                                                   add_chunk_key_transcripts)
+_gather_contigset_options = __add_gather_options("Output ContigSet XML file",
+                                                 "Chunk input JSON file",
+                                                 add_chunk_key_contigset)
+_gather_bigwig_options = __add_gather_options(
+    "Output BigWig file", "input BigWig file", add_chunk_key_bigwig)
+_gather_tgz_options = __add_gather_options(
+    "Output TGZ file", "input TGZ file", add_chunk_key_tgz)
+
+
+def __gather_runner(func, chunk_input_json, output_file, chunk_key, **kwargs):
+    chunks = load_pipeline_chunks_from_json(chunk_input_json)
+
+    # Allow looseness
+    if not chunk_key.startswith('$chunk.'):
+        chunk_key = '$chunk.' + chunk_key
+        log.warn(
+            "Prepending chunk key with '$chunk.' to '{c}'".format(c=chunk_key))
+
+    chunked_files = get_datum_from_chunks_by_chunk_key(chunks, chunk_key)
+    _ = func(chunked_files, output_file, **kwargs)
+    return 0
+
+
+def __rtc_gather_runner(func, rtc):
+    # Gather Resolved ToolContracts will have a chunk
+    return func(rtc.task.input_files[0], rtc.task.output_files[0], rtc.task.chunk_key)
+
+
+def __args_gather_runner(func, args):
+    return __gather_runner(func, args.chunk_json, args.output, args.chunk_key)
+
+# These make assumptions about the CLI argparser args labels (e.g.,
+# args.chunk_key)
+_args_runner_gather_fasta = P(__args_gather_runner, gather_fasta)
+_args_runner_gather_gff = P(__args_gather_runner, gather_gff)
+_args_runner_gather_vcf = P(__args_gather_runner, gather_vcf)
+_args_runner_gather_fastq = P(__args_gather_runner, gather_fastq)
+_args_runner_gather_fastq_contigset = P(
+    __args_gather_runner, gather_fastq_contigset)
+_args_runner_gather_fofn = P(__args_gather_runner, gather_fofn)
+_args_runner_gather_subreadset = P(__args_gather_runner, gather_subreadset)
+_args_runner_gather_alignmentset = P(__args_gather_runner, gather_alignmentset)
+_args_runner_gather_ccsset = P(__args_gather_runner, gather_ccsset)
+_args_runner_gather_ccs_alignmentset = P(
+    __args_gather_runner, gather_ccs_alignmentset)
+_args_runner_gather_contigset = P(__args_gather_runner, gather_contigset)
+_args_runner_gather_transcripts = P(__args_gather_runner, gather_transcripts)
+_args_runner_gather_csv = P(__args_gather_runner, gather_csv)
+_args_runner_gather_txt = P(__args_gather_runner, gather_txt)
+_args_runner_gather_report = P(__args_gather_runner, gather_report)
+_args_runner_gather_bigwig = P(__args_gather_runner, gather_bigwig)
+_args_runner_gather_tgz = P(__args_gather_runner, gather_tgz)
+
+# (chunk.json, output_file, chunk_key)
+run_main_gather_fasta = P(__gather_runner, gather_fasta)
+run_main_gather_fastq = P(__gather_runner, gather_fastq)
+run_main_gather_fastq_contigset = P(__gather_runner, gather_fastq_contigset)
+run_main_gather_csv = P(__gather_runner, gather_csv)
+run_main_gather_txt = P(__gather_runner, gather_txt)
+run_main_gather_report = P(__gather_runner, gather_report)
+run_main_gather_gff = P(__gather_runner, gather_gff)
+run_main_gather_vcf = P(__gather_runner, gather_vcf)
+run_main_gather_alignmentset = P(__gather_runner, gather_alignmentset)
+run_main_gather_subreadset = P(__gather_runner, gather_subreadset)
+run_main_gather_contigset = P(__gather_runner, gather_contigset)
+run_main_gather_ccsset = P(__gather_runner, gather_ccsset)
+run_main_gather_ccs_alignmentset = P(__gather_runner, gather_ccs_alignmentset)
+run_main_gather_transcripts = P(__gather_runner, gather_transcripts)
+run_main_gather_transcript_alignmentset = P(
+    __gather_runner, gather_transcript_alignmentset)
+run_main_gather_bigwig = P(__gather_runner, gather_bigwig)
+run_main_gather_tgz = P(__gather_runner, gather_tgz)
+run_main_gather_zip = P(__gather_runner, gather_zip)
