3b6b205121277ce59282cbf9e6605b0de3448833
natechols <nathaniel.echols@gmail.com>
2019-08-29 14:29:38 -0700

    [SL-4867] fix typo

--- pbcoretools/tasks2/auto_consolidate.py
+++ pbcoretools/tasks2/auto_consolidate.py
@@ -30,14 +30,14 @@ def _to_datastore(bam_file, bam_bai_file, datastore_json):
             uuid.uuid4(),
             "mapped_bam",
             FileTypes.BAM.file_type_id,
-            os.abspath(bam_file),
+            op.abspath(bam_file),
             name="Mapped BAM",
             description="Mapped reads in BAM format"),
         DataStoreFile(
             uuid.uuid4(),
             "mapped_bam_bai",
             FileTypes.BAMBAI.file_type_id,
-            os.abspath(bam_bai_file),
+            op.abspath(bam_bai_file),
             name="Mapped BAM Index",
             description="samtools index of mapped reads BAM")
     ]
