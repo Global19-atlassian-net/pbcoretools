a338ec6ce7ecb01851e42afd4ef0f2c67b05b739
David Seifert <dseifert@pacificbiosciences.com>
2019-11-04 09:59:30 -0800

    Simplify setup.py for Python 3

--- setup.py
+++ setup.py
@@ -1,50 +1,49 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from __future__ import print_function
-from setuptools import setup, Extension, find_packages
-import os.path
+from __future__ import absolute_import, division, print_function
+
+from setuptools import setup, find_packages
 import sys
 
-REQUIREMENTS_TXT = "requirements.txt"
+test_deps = [
+    'coverage',
+    'jinja2',
+    'jsonschema',
+    'nose',
+    'pbtestdata',
+    'pytest',
+    'sphinx',
+    'xmlbuilder',
+]
 
-if ("install" in sys.argv) and sys.version_info < (2, 7, 0):
-    print("pbcoretools requires Python 2.7")
-    sys.exit(-1)
-
-globals = {}
-execfile("pbcoretools/__init__.py", globals)
-__VERSION__ = globals["__VERSION__"]
-
-def _get_local_file(file_name):
-    return os.path.join(os.path.dirname(__file__), file_name)
-
-
-def _get_requirements(file_name):
-    with open(file_name, 'r') as f:
-        reqs = [line for line in f if not line.startswith("#")]
-    return reqs
-
-
-def _get_local_requirements(file_name):
-    return _get_requirements(_get_local_file(file_name))
-
+if sys.version_info[0] == 2:
+    test_deps += ['functools32']
 
 setup(
-    name = 'pbcoretools',
-    version=__VERSION__,
+    name='pbcoretools',
+    version='0.7.0',
     author='Pacific Biosciences',
     author_email='devnet@pacificbiosciences.com',
-    description="Python CLI tools and add-ons for reading and writing PacBio® data files",
-    license=open('LICENSES.txt').read(),
-    packages = find_packages('.'),
-    package_dir = {'':'.'},
-    zip_safe = False,
-    entry_points = {"console_scripts": [
-        "dataset = pbcoretools.dataset:main",
-        'pbvalidate = pbcoretools.pbvalidate.main:main',
-        'bamsieve = pbcoretools.bamsieve:main',
-        'pbtools-gather = pbcoretools.tasks2.gather:main'
-    ]},
-    install_requires=_get_local_requirements(REQUIREMENTS_TXT),
-    test_requires=("pbtestdata",))
+    description='Python CLI tools and add-ons for reading and writing PacBio® data files',
+    license='BSD-3-Clause-Clear',
+    packages=find_packages('.'),
+    include_package_data=True,
+    zip_safe=False,
+    entry_points={
+        'console_scripts': [
+            'dataset = pbcoretools.dataset:main',
+            'pbvalidate = pbcoretools.pbvalidate.main:main',
+            'bamsieve = pbcoretools.bamsieve:main',
+            'pbtools-gather = pbcoretools.tasks2.gather:main',
+        ]},
+    install_requires=[
+        'numpy >= 1.7.1',
+        'pysam >= 0.15.1',
+        'pbcore >= 1.7.0',
+        'pbcommand >= 0.6.7',
+    ],
+    test_requires=test_deps,
+    extras_require={
+        'test': test_deps},
+)
