13d55de909058d88e18bac22112dbe5bc837b128
Nathaniel Echols <nechols@pacificbiosciences.com>
2019-11-26 15:19:27 -0800

    update for pytest

--- tests/unit/test_cloud_utils.py
+++ tests/unit/test_cloud_utils.py
@@ -28,14 +28,14 @@ class TestCloudUtils(PbIntegrationBase):
         CHUNKS_OUT = [1, 2, 3, 3]
         for n_in, n_expected in zip(CHUNKS_IN, CHUNKS_OUT):
             nchunks = split_bam(bam_file1, n_in)
-            self.assertEqual(nchunks, n_expected)
+            assert nchunks == n_expected
             bam_in = IndexedBamReader(bam_file1)
             records_in = [rec.qName for rec in bam_in]
             records_out = []
             for i in range(n_expected):
                 bam_out = BamReader("reads.chunk%d.bam" % i)
                 records_out.extend([rec.qName for rec in bam_out])
-            self.assertEqual(records_in, records_out)
+            assert records_in == records_out
             self._remove_all()
 
     def test_get_zmw_bgzf_borders(self):
@@ -43,22 +43,20 @@ class TestCloudUtils(PbIntegrationBase):
         pbi_file = bam_file + ".pbi"
         pbi = PacBioBamIndex(pbi_file)
         offsets = get_zmw_bgzf_borders(pbi)
-        self.assertEqual(offsets, [(0, 1650, 396),
-                                   (16, 7247, 26575),
-                                   (48, 30983, 77209)])
+        assert offsets == [(0, 1650, 396), (16, 7247, 26575), (48, 30983, 77209)]
         bam_file = self._get_bam_path(self.DS2)
         pbi_file = bam_file + ".pbi"
         pbi = PacBioBamIndex(pbi_file)
         offsets = get_zmw_bgzf_borders(pbi)
-        self.assertEqual(offsets, [(0, 5177614, 447)])
+        assert offsets == [(0, 5177614, 447)]
 
     def test_get_bam_offsets(self):
         bam_file = self._get_bam_path(self.DS1)
         offsets = get_bam_offsets(bam_file, 4)
-        self.assertEqual(offsets, [396, 26575, 77209])
+        assert offsets == [396, 26575, 77209]
         offsets = get_bam_offsets(bam_file, 3)
-        self.assertEqual(offsets, [396, 26575, 77209])
+        assert offsets == [396, 26575, 77209]
         offsets = get_bam_offsets(bam_file, 2)
-        self.assertEqual(offsets, [396, 77209])
+        assert offsets == [396, 77209]
         offsets = get_bam_offsets(bam_file, 1)
-        self.assertEqual(offsets, [396])
+        assert offsets == [396]
