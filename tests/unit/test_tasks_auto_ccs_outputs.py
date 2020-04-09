3a0c0cbdcf6219330be47c6bb570d5ae8af81230
Nathaniel Echols <nechols@pacificbiosciences.com>
2020-04-09 13:01:52 -0700

    [SL-5606] modify task behavior to enable use in bam2fastx workflow

--- tests/unit/test_tasks_auto_ccs_outputs.py
+++ tests/unit/test_tasks_auto_ccs_outputs.py
@@ -25,8 +25,8 @@ class TestAutoCCSOutputs(PbIntegrationBase):
     INPUT_FILE = op.join(
         TESTDATA, "auto_ccs_outputs/m54006_180707_211919.consensusreadset.xml")
     OUTPUT_FILES = [
-        "m54006_180707_211919.Q20.fasta",
-        "m54006_180707_211919.Q20.fastq",
+        "m54006_180707_211919.Q20.fasta.zip",
+        "m54006_180707_211919.Q20.fastq.zip",
         "m54006_180707_211919.ccs.bam"
     ]
 
@@ -69,7 +69,7 @@ class TestAutoCCSOutputs(PbIntegrationBase):
     def test_export_sub_q20(self):
         ds_file = pbtestdata.get_file("rsii-ccs-multi-cell")
         tmp_dir = tempfile.mkdtemp()
-        files = run_ccs_bam_fastq_exports(ds_file, tmp_dir)
+        files = run_ccs_bam_fastq_exports(ds_file, tmp_dir, no_zip=True)
         assert len(files) == 5
         bam_file = op.basename(files[0].path)
         assert bam_file == "multiple_movies.ccs.bam"
@@ -87,6 +87,10 @@ class TestAutoCCSOutputs(PbIntegrationBase):
             records = [rec.id for rec in fasta_lq]
             assert records == ["m150404_101626_42267_c100807920800000001823174110291514_s1_p0/480/ccs",
                                "m150803_002149_42161_c100745121910000001823165807071563_s1_p0/137/ccs"]
+        # now with a lower cutoff
+        tmp_dir = tempfile.mkdtemp()
+        files = run_ccs_bam_fastq_exports(ds_file, tmp_dir, min_rq=0)
+        assert len(files) == 3, [f.path for f in files]
 
     def test_auto_ccs_outputs_barcoded(self):
         input_file = op.join(
@@ -111,3 +115,27 @@ class TestAutoCCSOutputs(PbIntegrationBase):
             ds = DataStore.load_from_json("output.datastore.json")
             self._check_datastore_files(ds.files.values(), [output_file])
             self._check_datastore_files_exist("output.datastore.json")
+
+    def test_auto_ccs_outputs_min_qv(self):
+        args = self._to_args(self.INPUT_FILE, "fastq") + ["--min-qv", "10"]
+        self._check_call(args)
+        ds = DataStore.load_from_json("output.datastore.json")
+        self._check_datastore_files(ds.files.values(),
+                                    ["m54006_180707_211919.Q10.fastq.zip"])
+
+    def test_auto_ccs_outputs_min_qv_2(self):
+        ds_file = pbtestdata.get_file("rsii-ccs-multi-cell")
+        args = [
+            "python", "-m",
+            "pbcoretools.tasks.auto_ccs_outputs",
+            "fasta",
+            ds_file,
+            "output.datastore.json"
+        ]
+        self._check_call(args)
+        ds = DataStore.load_from_json("output.datastore.json")
+        assert len(ds.files) == 2
+        args.extend(["--min-qv", "0"])
+        self._check_call(args)
+        ds = DataStore.load_from_json("output.datastore.json")
+        assert len(ds.files) == 1
